9450ee875346a180849564333adf0fd7cfe2d657
Bruno Sakakura <bruno.sakakura@gmail.com>
2022-09-11 22:46:14 +0900

    keymaps updated

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,4 +1,4 @@
-local has_lualine, lualine = pcall(require, 'lualine')
+local has_lualine, lualine = pcall(require, "lualine")
 if not has_lualine then return end
 
 local hide_in_width = function()
@@ -6,24 +6,24 @@ local hide_in_width = function()
 end
 
 local diagnostics = {
-  'diagnostics',
-  sources = { 'nvim_diagnostic' },
-  sections = { 'error', 'warn', 'info', 'hint' },
-  symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
+  "diagnostics",
+  sources = { "nvim_diagnostic" },
+  sections = { "error", "warn", "info", "hint" },
+  symbols = { error = " ", warn = " ", info = " ", hint = " " },
   colored = false,
   update_in_insert = true,
   always_visible = true,
 }
 
 local diff = {
-  'diff',
+  "diff",
   colored = false,
   symbols = { added = " ", modified = " ", removed = " " },
   cond = hide_in_width,
 }
 
 local branch = {
-  'branch',
+  "branch",
   icons_enabled = true,
   icon = "",
 }
@@ -32,25 +32,25 @@ lualine.setup({
   options = {
     icons_enabled = true,
     theme = "auto",
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
     disabled_filetypes = {},
     always_divide_middle = true,
     globalstatus = false,
   },
   sections = {
-    lualine_a = { 'mode' },
+    lualine_a = { "mode" },
     lualine_b = { branch, diff },
     lualine_c = {},
-    lualine_x = { 'encoding' },
+    lualine_x = { "encoding" },
     lualine_y = { diagnostics },
-    lualine_z = { 'progress' },
+    lualine_z = {},
   },
   inactive_sections = {
-    lualine_a = {},
+    lualine_a = { "filename" },
     lualine_b = {},
     lualine_c = {},
-    lualine_x = { 'location' },
+    lualine_x = {},
     lualine_y = {},
     lualine_z = {},
   },
